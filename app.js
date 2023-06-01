bd4a96cee0ef392dd20b7caaf25259bb8f5c55ca
DJ Patrias <Patrias21@gmail.com>
2023-06-01 14:02:24 -0700

    Finished

--- app.js
+++ app.js
@@ -1,4 +1,89 @@
-console.log("Hello World!\n==========\n");
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+// Exercise 1 
+
+function plus (num) {
+    return function (plusNumber) {
+        return num + plusNumber;
+    };
+  }
+
+
+const plus2 = plus(2);
+
+
+console.log(plus2(2));
+console.log (plus2(5));
+
+
+const plus100 = plus(100);
+
+console.log(plus100(100));
+console.log(plus100(5));
+
+
+// Exercise 2: forEach 
+
+let users = [
+    {
+      name: "Frodo",
+      age: 50,
+      score: 85,
+      isActive: false,
+    },
+    {
+      name: "Sam",
+      age: 38,
+      score: 94,
+      isActive: true,
+    },
+    {
+      name: "Merry",
+      age: 36,
+      score: 82,
+      isActive: true,
+    },
+    {
+      name: "Pippin",
+      age: 26,
+      score: 77,
+      isActive: false,
+    },
+  ];
+
+  users.forEach((user) => console.log(user.name));
+
+
+  // Exercise 3: map
+
+  let newUsers = users.map((user) => {
+      return { name: user.name, score: user.score };
+  });
+
+  console.log(newUsers) 
+
+  // Exercise 4: filter 
+
+  let activeUsers = users.filter((user) => user.isActive == true);
+    
+console.log(activeUsers); 
+
+// Exercise 5: sort
+
+ users.sort((a, b ) => {
+   if (a.score <  b.score) return 1 
+   else return -1;
+
+ }); 
+
+ console.log(users);
+
+ // Exercise 6: reduce 
+
+ let avgScore =  users.reduce((sum, user) => sum + user.score, 0) / users.length; 
+
+ console.log(avgScore);
+
+
+
+
+
